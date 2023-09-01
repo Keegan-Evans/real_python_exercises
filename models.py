93390be8bbb206c24814b83144ecb5925b525c76
Keegan Evans <keegan.evans@gmail.com>
2023-09-01 14:08:02 -0700

    MILESTONE: bookmark for adding environment file

--- models.py
+++ models.py
@@ -5,8 +5,55 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 author_publisher = Table(
-    'author_publisher',
+    "author_publisher",
     Base.metadata,
-    Column('author_id', Integer, ForeignKey('author.author_id')),
-    Column('publisher_id', Integer, ForeignKey('publisher.publisher_id'))
-    
+    Column("author_id", Integer, ForeignKey("author.author_id")),
+    Column("publisher_id", Integer, ForeignKey("publisher.publisher_id")),
+)
+
+book_publisher = Table(
+    "book_publisher",
+    Base.metadata,
+    Column("book_id", Integer, ForeignKey("book.book_id")),
+    Column("publisher_id", Integer, ForeignKey("publisher.publisher_id")),
+)
+
+
+class Author(Base):
+    __tablename__ = "author"
+    author_id = Column(Integer, primary_key=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    books = relationship("Book", backref="author")
+    publishers = relationship(
+        "Publisher", secondary=author_publisher, back_populates="authors"
+    )
+
+    # def __repr__(self):
+    # return f"<Author(author_id={self.author_id}, name={self.name})>"
+
+
+class Book(Base):
+    __tablename__ = "book"
+    book_id = Column(Integer, primary_key=True)
+    title = Column(String)
+    author_id = Column(Integer, ForeignKey("author.author_id"))
+    publishers = relationship(
+        "Publisher", secondary=book_publisher, back_populates="books"
+    )
+
+
+class Publisher(Base):
+    __tablename__ = "publisher"
+    publisher_id = Column(Integer, primary_key=True)
+    name = Column(String)
+    authors = relationship(
+        "Author", secondary=author_publisher, back_populates="publishers"
+    )
+
+    books = relationship(
+        "Book", secondary=book_publisher, back_populates="publishers"
+    )
+
+    # def __repr__(self):
+    # return f"<Book(book_id={self.book_id}, title={self.title})>"
